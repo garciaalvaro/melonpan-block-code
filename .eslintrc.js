fd6a0b3c6700ad1cbd184bdd5178b22541ceded9
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-05-23 16:19:29 +0200

    Added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+	// For type definition files we use the TypeScript parser,
+	// as babel parser doesn't seem to work correctly.
+	overrides: [
+		{
+			parser: "@typescript-eslint/parser",
+			files: ["*.d.ts"],
+		},
+	],
+
+	parser: "babel-eslint",
+
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:prettier/recommended",
+		"prettier/@typescript-eslint",
+		"prettier/react",
+	],
+
+	env: {
+		es2020: true,
+		browser: true,
+		node: true,
+	},
+
+	rules: {
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/camelcase": "off",
+		"@typescript-eslint/no-var-requires": "off",
+		"@typescript-eslint/ban-ts-ignore": "off",
+		"react/react-in-jsx-scope": "off",
+		"react/prop-types": "off",
+		"react/display-name": "off",
+	},
+
+	settings: {
+		react: {
+			version: "16.9.0", // Version used in WP 5.4.1
+		},
+	},
+};
