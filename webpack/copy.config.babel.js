a985d3feb5a30927217bd9e126836a5780f859e6
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-05-24 13:28:31 +0200

    Fixed copy-webpack-plugin to the latest version way

--- webpack/copy.config.babel.js
+++ webpack/copy.config.babel.js
@@ -1,39 +1,43 @@
 import CopyPlugin from "copy-webpack-plugin";
 import path from "path";
 
+const ROOT_DIR = path.join(__dirname, "..");
+
 export default {
 	entry: path.join(__dirname, "copy.entry.js"),
 
 	output: {
-		path: path.join(__dirname, "../_release"),
+		path: ROOT_DIR + "/_release",
 		filename: "_temp.js",
 	},
 
 	plugins: [
-		new CopyPlugin([
-			{
-				from: "**/*",
+		new CopyPlugin({
+			patterns: [
+				{
+					from: ROOT_DIR + "/**/*",
 
-				ignore: [
-					".*",
-					".*/**",
-					"_extras/**",
-					"_release/**",
-					"_temp.js",
-					"assets-repo/**",
-					"enzyme.config.js",
-					"jest*",
-					"node_modules/**",
-					"package.json",
-					"package-lock.json",
-					"pro/**",
-					"README.md",
-					"src/**",
-					"tsconfig.json",
-					"types.d.ts",
-					"webpack/**",
-				],
-			},
-		]),
+					globOptions: {
+						ignore: [
+							ROOT_DIR + "/_extras/**",
+							ROOT_DIR + "/_release/**",
+							ROOT_DIR + "/_temp.js",
+							ROOT_DIR + "/assets-repo/**",
+							ROOT_DIR + "/enzyme.config.js",
+							ROOT_DIR + "/jest*",
+							ROOT_DIR + "/node_modules/**",
+							ROOT_DIR + "/package.json",
+							ROOT_DIR + "/package-lock.json",
+							ROOT_DIR + "/pro/**",
+							ROOT_DIR + "/README.md",
+							ROOT_DIR + "/src/**",
+							ROOT_DIR + "/tsconfig.json",
+							ROOT_DIR + "/types.d.ts",
+							ROOT_DIR + "/webpack/**",
+						],
+					},
+				},
+			],
+		}),
 	],
 };
