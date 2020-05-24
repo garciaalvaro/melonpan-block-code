22993870e6bd301cecb69f4e73e3bfe05a2e3a19
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-05-24 13:30:55 +0200

    Updated version

--- melonpan-block-code.php
+++ melonpan-block-code.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://wordpress.org/plugins/melonpan-block-code/
  * Description: Block to display code, with highlighted syntax, which can be copied to the clipboard.
  * Author: melonpan
- * Version: 1.0.1
+ * Version: 1.1.0
  * License: GPL3+
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  */
@@ -15,7 +15,7 @@ namespace MELONPANBLOCKCODE;
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 if ( ! defined( __NAMESPACE__ . '\PLUGIN_VERSION' ) ) {
-	define( __NAMESPACE__ . '\PLUGIN_VERSION', '1.0.1' );
+	define( __NAMESPACE__ . '\PLUGIN_VERSION', '1.1.0' );
 }
 if ( ! defined( __NAMESPACE__ . '\PLUGIN_NAME' ) ) {
 	define( __NAMESPACE__ . '\PLUGIN_NAME', 'melonpan-block-code' );
